0ba99b6a39ed4bd3da865fbe6699670553a1c0d8
judorsch <jdorsch@udel.edu>
2021-10-07 12:27:54 -0400

    finish up component

--- src/Components/Counters.tsx
+++ src/Components/Counters.tsx
@@ -1,9 +1,18 @@
 import React from "react";
 
-export const Counters = ({text, num}: {text:string, num:number}):JSX.Element =>{
+export const Counters = ({text, num, setNum}: {text:string, num:number, setNum:(num:number) => void}):JSX.Element =>{
+    function updateScore(text:string):void{
+        if(text === "+"){
+            setNum(num + 1);
+        } else{
+            setNum(num - 1);
+        }
+    }
     return(
         <div>
-            <button>HI</button>
+            <form>{text}: {num}</form>
+            <button onClick = {()=>updateScore("+")}>Increase Score </button>
+            <button onClick = {()=>updateScore("-")}>Decrease Score </button>
         </div>
     );
 
