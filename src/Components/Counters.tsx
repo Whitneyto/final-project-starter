4bd4c2953077ee7013957b4a174adf84f927acc9
judorsch <jdorsch@udel.edu>
2021-10-07 12:24:32 -0400

    start of new component

---
+++ src/Components/Counters.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Counters = ({text, num}: {text:string, num:number}):JSX.Element =>{
+    return(
+        <div>
+            <button>HI</button>
+        </div>
+    );
+
+
+};
